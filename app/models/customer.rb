db107ef658ead0daadae16f6066f72d4dc72fec0
Dan Roberts <dan@adadevelopersacademy.org>
2018-06-18 17:54:20 -0700

    Fix movies checked out count. Not sure why it wasn't working before

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,10 @@ class Customer < ApplicationRecord
   has_many :movies, through: :rentals
 
   def movies_checked_out_count
-    self.rentals.where(returned: false).length
+    count = 0
+    self.rentals.each do |r|
+      count += 1 unless r.returned
+    end
+    return count
   end
 end
